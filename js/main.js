9b08179bc4819526526d879aec86c05148430e3c
Mahesh Jethva <mjethvar8@gmail.com>
2017-04-23 18:16:47 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+//stAuth10065: I Mahesh Jethva, 00327510 certify that this material is my original work. 
+//No other person's work has been used without due acknowledgement. 
+//I have not made my work available to anyone else.
